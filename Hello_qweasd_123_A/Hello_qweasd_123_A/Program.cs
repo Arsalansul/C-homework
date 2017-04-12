451825b54d4a555ed239f5904c55418b50dfab7e
Arsalan Sultimov <arsalan.syltimov@katiypushnina.office.elefun-games.com>
2017-04-12 16:39:32 +0700

    hello world

---
+++ Hello_qweasd_123_A/Hello_qweasd_123_A/Program.cs
@@ -0,0 +1,28 @@
+﻿// A Hello World! program in C#.
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Deathpool gamers!");
+            Console.WriteLine();
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            int a;
+            a = 5;
+            int b, c;
+            bool d;
+            d = true;
+            long e = 10;
+            float f = 5.5f;
+            char g = 'g';
+
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
