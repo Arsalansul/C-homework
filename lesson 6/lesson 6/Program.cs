3e40b863d6824cc18ecb3794e5099e426af63311
Arsalan Sultimov <arsalan.syltimov@katiypushnina.office.elefun-games.com>
2017-04-14 00:22:53 +0700

    дз не доделал

--- lesson 6/lesson 6/Program.cs
+++ lesson 6/lesson 6/Program.cs
@@ -10,6 +10,34 @@ namespace lesson_6
     {
         static void Main(string[] args)
         {
+            //первая задача
+            for (int i = 0; i < 20; i++)
+                Console.WriteLine(1 + i * 3);
+            Console.ReadKey();
+
+            //вторая задача
+            bool b = false;
+            string[] password;
+            while (b == false)
+            {
+                Console.WriteLine("введите пароль");
+                password = Console.ReadLine();
+                if()
+            }
+            Console.WriteLine("спасибо!");
+
+            //третья задача
+
+
+            int[] numbers = {1,2,3,4,5,6,7,8,9 };
+            int s=0;
+            for (int i = 0; i < numbers.Length; i++ )
+                s += numbers[i];
+            Console.WriteLine(s);
+            Console.ReadKey();
+
+            bool b = false;
+
         }
     }
 }
