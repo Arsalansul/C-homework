b03d327ec736af6dc63cd78dd25565550a47c2f9
Arsalan Sultimov <arsalan.syltimov@katiypushnina.office.elefun-games.com>
2017-04-14 20:54:18 +0700

    l 7 done

---
+++ lesson 7/lesson 7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1,2,3,40,55};
+            int s = 0;
+            foreach (int n in numbers)
+            {
+                if (n > 20 && n < 50)
+                    Console.WriteLine(n);
+            }
+            //Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
