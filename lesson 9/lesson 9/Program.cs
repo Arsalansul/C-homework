d5927e30914d8f52f84e0a9cee8fbb8bad3e5f7f
DESKTOP-M72LUD9\arsalansul <arsalansul@gmail.com>
2017-04-17 03:23:15 +0700

    l 9

---
+++ lesson 9/lesson 9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello";
+            char c = s1[1];
+            Console.WriteLine("\n"+s1);
+            string.IsNullOrEmpty(s1);
+            string s2 = "world";
+            int a = string.Compare(s1, s2);
+            Console.WriteLine(a);
+            Console.WriteLine(s1.ToUpper());
+            Console.WriteLine(s2.ToLower());
+            Console.ReadKey();
+        }
+    }
+}
