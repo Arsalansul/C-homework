3214c5e9bc7f602975c6fedc2e76e19ec7d4f96f
DESKTOP-M72LUD9\arsalansul <arsalansul@gmail.com>
2017-04-18 03:52:52 +0700

    l9 done

--- lesson 9/lesson 9/Program.cs
+++ lesson 9/lesson 9/Program.cs
@@ -19,7 +19,47 @@ namespace lesson_9
             Console.WriteLine(a);
             Console.WriteLine(s1.ToUpper());
             Console.WriteLine(s2.ToLower());
+            
+            if (s1.Contains("Hello"))
+                Console.WriteLine("Yes");
+
+            Console.WriteLine(s1.IndexOf("lo"));
+            Console.WriteLine(s1.StartsWith("He"));
+
+            Console.WriteLine(s1.Insert(2, "123"));
+
+            Console.WriteLine(s1.Remove(3));
+            Console.WriteLine(s1.Remove(3, 1));
+
+            Console.WriteLine(s1.Substring(2));
+            Console.WriteLine(s1.Substring(2, 2));
+
+            Console.WriteLine(s1.Replace("l", "S"));
+
+            char[] array = s1.ToCharArray();
+            Console.WriteLine(array[1]);
+
+            string[] split = s1.Split('e');
+            Console.WriteLine(split[1]);
             Console.ReadKey();
+
+            //1st task
+            string s = "Nikolay,Nikolay,Nikolay,Nikolay,Nikolay,Pavel";
+            Console.WriteLine(s.Replace("Nikolay", "Oleg"));
+            Console.ReadKey();
+
+            //2nd task
+            string text = "Сегодня мы с вами рассмотрели, как работать со строками в Си-шарп. Были111 описаны основные операторы и методы, которые используются для работы со строками";
+            Console.WriteLine(text.Substring(text.IndexOf("Были")));
+            Console.ReadKey();
+
+            //3rd task
+            string logins = "Login1,LOgin2,login3,loGin4";
+            string loginsLow = logins.ToLower();
+            string[] login = loginsLow.Split(',');
+            Console.WriteLine(login[1]);
+            Console.ReadKey();
+
         }
     }
 }
