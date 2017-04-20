55c4cf54013b20a23f27c2349e5864bde4312f94
Arsalan Sultimov <arsalan.syltimov@katiypushnina.office.elefun-games.com>
2017-04-20 20:25:35 +0700

    l10 done

---
+++ lesson 10/lesson 10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+            Console.WriteLine("Enter number");
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                result = "вы ввели" + a;
+
+            }
+            catch (FormatException)
+            {
+                result = "ввели не число";
+            }
+            finally
+            {
+                Console.WriteLine(result);
+            }
+            
+
+            //HW
+            int[] array = {0,1,2,3,4,5,6,7,8,9};
+            Console.WriteLine("введите 1 число");
+            try
+            {
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("введите 2 число");
+                int j = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(array[i] + array[j]);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели не число");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("индекс вне рамок массива"); 
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
