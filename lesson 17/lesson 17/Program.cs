3af0d83cb26f31f13ea858919960ff0d55e2d388
DESKTOP-M72LUD9\arsalansul <arsalansul@gmail.com>
2017-05-02 05:16:21 +0700

    l 17 done

--- lesson 17/lesson 17/Program.cs
+++ lesson 17/lesson 17/Program.cs
@@ -9,9 +9,17 @@ namespace lesson_17
     class Animal
     {
         public string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
     }
     class Dog : Animal
     {
+        public Dog(string name):base(name)
+        {
+
+        }
         public void Guad()
         {
             // собака охраняет
@@ -19,15 +27,19 @@ namespace lesson_17
     }
     class Cat : Animal
     {
+        public Cat(string name):base(name)
+        {
+
+        }
         public void CatchMouse()
         {
             // кошка ловит мышь
         }
     }
-    class Parrot
+    class Parrot:Animal
     {
         public double BeakLenght { get; set; }
-        public Parrot(string name, double beak)
+        public Parrot(string name, double beak): base(name)
         {            
             BeakLenght = beak;
         }
@@ -35,22 +47,38 @@ namespace lesson_17
 
     class GeoFigure
     {
-        private double[,] Center;
-        public GeoFigure(double[,] x)
+        public double[,] Center { get; set; }
+        public GeoFigure(double[,] center)
         {
-            Center=x;
+            Center=center;
         } 
     }
-    class Triangle
+    class Triangle:GeoFigure
     {
+        private double[,] A;
+        private double[,] B;
 
-    }
-    class Circle
-    {
-        public double Radius{get;set;};
-        public Circle(double[,] x, double radius)
+        public Triangle(double[,] center, double[,] a, double[,] b):base(center)
         {
-            Radius=radius;
+            A = a;
+            B = b;
+        }
+        public void Draw()
+        {
+            //могла бы быть специфическая логика рисования фигуры
+        }
+    }
+    class Circle:GeoFigure
+    {
+        private double Radius { get; set; }
+        
+        public Circle(double[,] center, double radius):base(center)
+        {
+            Radius=radius;            
+        }
+        public void Draw()
+        {
+            //могла бы быть специфическая логика рисования фигуры
         }
         
     }
@@ -60,14 +88,19 @@ namespace lesson_17
     {
         static void Main(string[] args)
         {
-            Dog dog1 = new Dog();
-            dog1.Name = "Barbos";
-            Cat cat1 = new Cat();
-            cat1.Name = "Murzik";
+            Dog dog1 = new Dog("Barbos");
+            //dog1.Name = "Barbos";
+            Cat cat1 = new Cat("Murzik");
+            //cat1.Name = "Murzik";
             dog1.Guad();
             cat1.CatchMouse();
             Parrot parrot1 = new Parrot("Kesha", 2.2);
             Console.WriteLine(parrot1.BeakLenght);
+
+            Circle circle1 = new Circle(new double [1,1], 1);
+            Triangle triangle1 = new Triangle(new double[5, 5], new double[6, 5], new double[5, 6]);
+            circle1.Draw();
+            triangle1.Draw();
             Console.ReadKey();
         }
     }
