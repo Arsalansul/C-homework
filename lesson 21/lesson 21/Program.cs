4182d2a04679e0a0e74e253d14cc869456bfa259
DESKTOP-M72LUD9\arsalansul <arsalansul@gmail.com>
2017-05-02 22:31:47 +0700

    l21 done

---
+++ lesson 21/lesson 21/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_21
+{
+    abstract class Animal
+    {
+        public string Name { get; set; }
+        public string Type { get; protected set; }
+        public abstract void GetInfo();
+    }
+    class Parrot:Animal
+    {
+        public Parrot(string name)
+        {
+            Name = name;
+            Type = "Bird";
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine("Type: "+Type + "\n"+"Name: " + Name+"\n");
+        }
+    }
+    class Cat:Animal
+    {
+        public Cat(string name)
+        {
+            Name = name;
+            Type = "Cat";
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine("Type: " + Type + "\n" + "Name: " + Name+"\n");
+        }
+    }
+
+    abstract class Human
+    {
+        public string Name { get; set; }
+        public string Nation { get; protected set; }
+        public abstract void SayHello();
+    }
+    class Russian:Human
+    {
+        public Russian(string name)
+        {
+            Nation = "Russian";
+            Name = name;
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine(Nation+": Privet " + Name + "\n");
+        }
+    }
+    class Amirican:Human
+    {
+        public Amirican(string name)
+        {
+            Nation = "American";
+            Name = name;
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine(Nation + ": Hello " + Name + "\n");
+        }
+    }
+        
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Parrot("Kesha"));
+            animals.Add(new Cat("Murzik"));
+            foreach(Animal animal in animals)
+            {
+                animal.GetInfo();                
+            }
+            List<Human> humans = new List<Human>();
+            humans.Add(new Russian("Pasha"));
+            humans.Add(new Amirican("Sam"));
+            foreach (Human human in humans)
+                human.SayHello();
+            Console.ReadKey();
+        }
+    }
+}
