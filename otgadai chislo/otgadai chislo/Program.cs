8b8b54033f64168f38f4df3cab5d29fbae951d26
Arsalan Sultimov <arsalan.syltimov@katiypushnina.office.elefun-games.com>
2017-05-11 16:52:57 +0700

    доделал задачу

--- otgadai chislo/otgadai chislo/Program.cs
+++ otgadai chislo/otgadai chislo/Program.cs
@@ -8,30 +8,68 @@ namespace otgadai_chislo
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Move()
         {
-            Random rnd1 = new Random();
-            int CompNumber=22;
-            int MyNumber=23;
-            while (MyNumber != CompNumber)
+            Console.WriteLine("Enter your number (1-20): ");
+
+            while (true)
             {
-                CompNumber = rnd1.Next(1, 21);
-                Console.WriteLine("Enter your number (1-20): ");
-                bool numberIsCorrect = false;
-                while (numberIsCorrect == false)
-                {
                     try
                     {
-                        MyNumber = Convert.ToInt32(Console.ReadLine());
-                        numberIsCorrect = true;
+                        var Number = Convert.ToInt32(Console.ReadLine());
+
+                        if (Number > 20 || Number < 1) Console.WriteLine("Число должно быть от 1 до 20. Повтори попытку: ");
+
+                        else return Number;
                     }
                     catch
                     {
                         Console.WriteLine("Error! Enter only your number: ");
                     }
-                }
             }
-            Console.WriteLine("Congratulations!");
+        }
+
+        public static bool Proverka(int FirstPlayerNumber, int SecondPlayerNumber)
+        {
+            if (FirstPlayerNumber == SecondPlayerNumber)
+                return true;
+            else
+                return false;            
+        }
+
+        static void Main(string[] args)
+        {
+            Random rnd1 = new Random();
+            int CompNumber = rnd1.Next(1, 21);
+            Console.WriteLine(CompNumber);
+            int MyNumber=Move();
+            int Number;
+            bool MyMove = false;
+            bool End = false;
+            while (!End)
+            {
+                if (MyMove)
+                {
+                    Console.WriteLine("Попробуй отгадать");
+                    Number = Move();
+                    MyMove=false;
+                    End = Proverka(Number, CompNumber);
+
+                }
+                else
+                {
+                    Number=rnd1.Next(1, 21);
+                    Console.WriteLine("Очередь второго игрока. Его попытка " + Number);
+                    End = Proverka(Number, MyNumber);
+                    MyMove=true;
+                }
+
+            }
+            
+            if (MyMove)
+                Console.WriteLine("Lose");
+            else
+                Console.WriteLine("Congratulations!");
             Console.ReadKey();
         }
     }
