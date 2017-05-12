72b19108c699a2ed68e2372c5563861a9a62d4ad
Arsalan Sultimov <arsalan.syltimov@katiypushnina.office.elefun-games.com>
2017-05-12 14:34:50 +0700

    l23 done

---
+++ lesson 23/lesson 23/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_23
+{
+    class Program
+    {
+        public static void AddAndDisplay(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void AddAndDisplay(char a, char b)
+        {
+            Console.WriteLine(a.ToString() + b.ToString());
+        }
+
+        public static void AddAndDisplayNames(List<string> names)
+        {
+            foreach (string name in names)
+            {
+                Console.Write(name + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void AddAndDisplayNames(List<string> names, char a)
+        {
+            foreach (string name in names)
+            {
+                Console.Write(name + a.ToString());
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            AddAndDisplay(2, 3);
+            AddAndDisplay('c', '#');
+            List<string> Names = new List<string> {"asd","fgh", "jkl"};
+            AddAndDisplayNames(Names);
+            AddAndDisplayNames(Names,'-');
+            Console.ReadKey();
+        }
+    }
+}
